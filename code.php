5d1826b067eea0f494ef6c788a0cc72bd96e2ce5
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-07-12 14:00:45 +0100

    load update student data using ajax script

--- code.php
+++ code.php
@@ -71,5 +71,47 @@ if(isset($_GET['student_id']))
         return;
     }
 }
+if(isset($_POST['update_student']))
+{
+    $student_id = mysqli_real_escape_string($con, $_POST['student_id']);
+
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $course = mysqli_real_escape_string($con, $_POST['course']);
+
+    if($name == NULL || $email == NULL || $phone == NULL || $course == NULL)
+    {
+        $res = [
+            'status' => 422,
+            'message' => 'All fields are mandatory'
+        ];
+        echo json_encode($res);
+        return;
+    }
+
+    $query = "UPDATE students SET name='$name', email='$email', phone='$phone', course='$course' 
+                WHERE id='$student_id'";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $res = [
+            'status' => 200,
+            'message' => 'Student Updated Successfully'
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 500,
+            'message' => 'Student Not Updated'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
 
 
--- student.php
+++ student.php
@@ -255,6 +255,45 @@
             });
 
         });
+        $(document).on('submit', '#updateStudent', function (e) {
+            e.preventDefault();
+
+            var formData = new FormData(this);
+            formData.append("update_student", true);
+
+            $.ajax({
+                type: "POST",
+                url: "code.php",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    
+                    var res = jQuery.parseJSON(response);
+                    if(res.status == 422) {
+                        $('#errorMessageUpdate').removeClass('d-none');
+                        $('#errorMessageUpdate').text(res.message);
+
+                    }else if(res.status == 200){
+
+                        $('#errorMessageUpdate').addClass('d-none');
+
+                        alertify.set('notifier','position', 'top-right');
+                        alertify.success(res.message);
+                        
+                        $('#studentEditModal').modal('hide');
+                        $('#updateStudent')[0].reset();
+
+                        $('#myTable').load(location.href + " #myTable");
+
+                    }else if(res.status == 500) {
+                        alert(res.message);
+                    }
+                }
+            });
+
+        });
+
         </script>
     </body>
 </html>
