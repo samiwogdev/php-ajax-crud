ed82adf6c569f8149cb6b03ead1956e72b882a84
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-07-12 14:38:10 +0100

    delete student with ajax loaded

--- code.php
+++ code.php
@@ -113,5 +113,31 @@ if(isset($_POST['update_student']))
         return;
     }
 }
+if(isset($_POST['delete_student']))
+{
+    $student_id = mysqli_real_escape_string($con, $_POST['student_id']);
+
+    $query = "DELETE FROM students WHERE id='$student_id'";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $res = [
+            'status' => 200,
+            'message' => 'Student Deleted Successfully'
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 500,
+            'message' => 'Student Not Deleted'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
 
 
--- student.php
+++ student.php
@@ -293,6 +293,35 @@
             });
 
         });
+ $(document).on('click', '.deleteStudentBtn', function (e) {
+            e.preventDefault();
+
+            if(confirm('Are you sure you want to delete this data?'))
+            {
+                var student_id = $(this).val();
+                $.ajax({
+                    type: "POST",
+                    url: "code.php",
+                    data: {
+                        'delete_student': true,
+                        'student_id': student_id
+                    },
+                    success: function (response) {
+
+                        var res = jQuery.parseJSON(response);
+                        if(res.status == 500) {
+
+                            alert(res.message);
+                        }else{
+                            alertify.set('notifier','position', 'top-right');
+                            alertify.success(res.message);
+
+                            $('#myTable').load(location.href + " #myTable");
+                        }
+                    }
+                });
+            }
+        });
 
         </script>
     </body>
