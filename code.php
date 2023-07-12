385ffa4f07f03c250192a59bc56ee1301ec8282f
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-07-12 13:22:03 +0100

    loaded fetch student by id method using ajax

--- code.php
+++ code.php
@@ -42,4 +42,34 @@ if(isset($_POST['save_student_but']))
     }
 }
 
+if(isset($_GET['student_id']))
+{
+    $student_id = mysqli_real_escape_string($con, $_GET['student_id']);
+
+    $query = "SELECT * FROM students WHERE id='$student_id'";
+    $query_run = mysqli_query($con, $query);
+
+    if(mysqli_num_rows($query_run) == 1)
+    {
+        $student = mysqli_fetch_array($query_run);
+
+        $res = [
+            'status' => 200,
+            'message' => 'Student Fetch Successfully by id',
+            'data' => $student
+        ];
+        echo json_encode($res);
+        return;
+    }
+    else
+    {
+        $res = [
+            'status' => 404,
+            'message' => 'Student Id Not Found'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+
 
--- student.php
+++ student.php
@@ -227,6 +227,34 @@
                 
             });
     });
+        $(document).on('click', '.editStudentBtn', function () {
+
+            var student_id = $(this).val();
+            
+            $.ajax({
+                type: "GET",
+                url: "code.php?student_id=" + student_id,
+                success: function (response) {
+
+                    var res = jQuery.parseJSON(response);
+                    if(res.status == 404) {
+
+                        alert(res.message);
+                    }else if(res.status == 200){
+
+                        $('#student_id').val(res.data.id);
+                        $('#name').val(res.data.name);
+                        $('#email').val(res.data.email);
+                        $('#phone').val(res.data.phone);
+                        $('#course').val(res.data.course);
+
+                        $('#studentEditModal').modal('show');
+                    }
+
+                }
+            });
+
+        });
         </script>
     </body>
 </html>
